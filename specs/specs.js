d1d9e93ef93ee7d37b925ebd6fff9ae0b1aac7dd
Nuatu Tseggai <student@epicodus.com>
2014-07-26 17:25:33 -0700

    update specs to cover randomized functionality

--- specs/specs.js
+++ specs/specs.js
@@ -1,23 +1,47 @@
-describe('coinCombinations', function(){
+describe('LEAST coinCombinations', function(){
 
  	it('takes the number 1 and returns the proper change', function(){
-    coinCombinations(1).slice(0,45).should.equal("0 quarters, 0 dimes, 0 nickels, and 1 pennies");
+    coinCombinations(1)[0].should.equal("0 quarters, 0 dimes, 0 nickels, and 1 pennies");
   });
 
 	it('takes the number 5 and returns the proper change', function(){
-    coinCombinations(5).slice(0,45).should.equal("0 quarters, 0 dimes, 1 nickels, and 0 pennies");
+    coinCombinations(5)[0].should.equal("0 quarters, 0 dimes, 1 nickels, and 0 pennies");
   });
 
   it('takes the number 10 and returns the proper change', function(){
-    coinCombinations(10).slice(0,45).should.equal("0 quarters, 1 dimes, 0 nickels, and 0 pennies");
+    coinCombinations(10)[0].should.equal("0 quarters, 1 dimes, 0 nickels, and 0 pennies");
   });
 
 	it('takes the number 25 and returns the proper change', function(){
-    coinCombinations(25).slice(0,45).should.equal("1 quarters, 0 dimes, 0 nickels, and 0 pennies");
+    coinCombinations(25)[0].should.equal("1 quarters, 0 dimes, 0 nickels, and 0 pennies");
   });
 
   it('takes the number 99 and returns the proper change', function(){
-    coinCombinations(99).slice(0,45).should.equal("3 quarters, 2 dimes, 0 nickels, and 4 pennies");
+    coinCombinations(99)[0].should.equal("3 quarters, 2 dimes, 0 nickels, and 4 pennies");
+  });
+
+});
+
+describe('RANDOM coinCombinations', function(){
+
+  it('takes the number 1 and returns the proper change', function(){
+    coinCombinations(1)[1].should.equal("0 quarters, 0 dimes, 0 nickels, and 1 pennies");
+  });
+
+  it('takes the number 5 and returns the proper change', function(){
+    coinCombinations(5)[1].should.not.equal("0 quarters, 0 dimes, 1 nickels, and 0 pennies");
+  });
+
+  it('takes the number 10 and returns the proper change', function(){
+    coinCombinations(10)[1].should.not.equal("0 quarters, 1 dimes, 0 nickels, and 0 pennies");
+  });
+
+  it('takes the number 25 and returns the proper change', function(){
+    coinCombinations(25)[1].should.not.equal("1 quarters, 0 dimes, 0 nickels, and 0 pennies");
+  });
+
+  it('takes the number 99 and returns the proper change', function(){
+    coinCombinations(99)[1].should.not.equal("3 quarters, 2 dimes, 0 nickels, and 4 pennies");
   });
 
 });

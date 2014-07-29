3ee3d75a9a1ad4c22a7d0a46e2abc60c423d5e0c
Nuatu <nuatut@gmail.com>
2014-07-28 20:19:35 -0700

    better describe specs

--- specs/specs.js
+++ specs/specs.js
@@ -1,47 +1,31 @@
 describe('LEAST coinCombinations', function(){
-
- 	it('takes the number 1 and returns the proper change', function(){
+ 	it('returns proper qty of pennies', function(){
     coinCombinations(1)[0].should.equal("0 quarters, 0 dimes, 0 nickels, and 1 pennies");
   });
 
-	it('takes the number 5 and returns the proper change', function(){
+	it('returns proper qty of nickels', function(){
     coinCombinations(5)[0].should.equal("0 quarters, 0 dimes, 1 nickels, and 0 pennies");
   });
 
-  it('takes the number 10 and returns the proper change', function(){
+  it('returns proper qty of dimes', function(){
     coinCombinations(10)[0].should.equal("0 quarters, 1 dimes, 0 nickels, and 0 pennies");
   });
 
-	it('takes the number 25 and returns the proper change', function(){
+	it('returns proper qty of quarters', function(){
     coinCombinations(25)[0].should.equal("1 quarters, 0 dimes, 0 nickels, and 0 pennies");
   });
 
-  it('takes the number 99 and returns the proper change', function(){
+  it('returns proper change', function(){
     coinCombinations(99)[0].should.equal("3 quarters, 2 dimes, 0 nickels, and 4 pennies");
   });
-
 });
 
 describe('RANDOM coinCombinations', function(){
-
-  it('takes the number 1 and returns the proper change', function(){
+  it('returns proper qty of pennies', function(){
     coinCombinations(1)[1].should.equal("0 quarters, 0 dimes, 0 nickels, and 1 pennies");
   });
 
-  it('takes the number 5 and returns the proper change', function(){
-    coinCombinations(5)[1].should.not.equal("0 quarters, 0 dimes, 1 nickels, and 0 pennies");
-  });
-
-  it('takes the number 10 and returns the proper change', function(){
-    coinCombinations(10)[1].should.not.equal("0 quarters, 1 dimes, 0 nickels, and 0 pennies");
-  });
-
-  it('takes the number 25 and returns the proper change', function(){
-    coinCombinations(25)[1].should.not.equal("1 quarters, 0 dimes, 0 nickels, and 0 pennies");
-  });
-
-  it('takes the number 99 and returns the proper change', function(){
+  it('returns proper RANDOM qty of change equal to input', function(){
     coinCombinations(99)[1].should.not.equal("3 quarters, 2 dimes, 0 nickels, and 4 pennies");
   });
-
 });
